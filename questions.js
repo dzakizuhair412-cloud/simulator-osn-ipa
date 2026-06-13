abafe60f30bf75302d36f8470a3b6d594eca4f39
dzakizuhair412-cloud <dzakizuhair412@gmail.com>
2026-06-13 19:15:05 +0700

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,42 @@
+const questionBank = {
+
+1:[
+
+{
+field:"Biologi",
+question:"Organel tempat respirasi sel adalah...",
+options:[
+"Ribosom",
+"Lisosom",
+"Mitokondria",
+"Badan Golgi"
+],
+answer:2
+},
+
+{
+field:"Fisika",
+question:"Sebuah benda bermassa 2 kg dipercepat 3 m/s². Gaya yang bekerja adalah...",
+options:[
+"3 N",
+"5 N",
+"6 N",
+"9 N"
+],
+answer:2
+},
+
+{
+field:"Kimia",
+question:"Nomor atom karbon adalah...",
+options:[
+"4",
+"6",
+"8",
+"12"
+],
+answer:1
+}
+
+]
+};
